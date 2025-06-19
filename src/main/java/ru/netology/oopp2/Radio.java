e321f5107135e39cc5ee8bdde2d83a0398c5615b
Ilich8603 <dremin86ilja@gmail.com>
2025-06-19 15:59:02 +0600

    Add Radio station

--- src/main/java/ru/netology/oopp2/Radio.java
+++ src/main/java/ru/netology/oopp2/Radio.java
@@ -2,10 +2,36 @@ package ru.netology.oopp2;
 
 public class Radio {
 
+    private int currentStationNumber;
+
     private int currentVolume;
+
+    public int getCurrentStationNumber() {
+        return currentStationNumber;
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
+    }
+
+    public void setCurrentStationNumber (int newStationNumber) {
+        if (newStationNumber < 0) {
+            setMaxStationNumber();
+            return;
+        }
+        if (newStationNumber > 9) {
+            setMinStationNumber();
+            return;
+        }
+        currentStationNumber = newStationNumber;
+    }
+
+    public void setMinStationNumber() {
+        currentStationNumber = 0;
+    }
+
+    public void setMaxStationNumber() {
+        currentStationNumber = 9;
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
@@ -28,9 +54,27 @@ public class Radio {
         currentVolume = 100;
     }
 
+    public void nextStation() {
+        if (currentStationNumber < 9) {
+            currentStationNumber ++;
+        }
+        else {
+            setMinStationNumber();
+        }
+    }
+
     public void increaseVolume() {
         if (currentVolume < 100) {
             currentVolume ++;
+        }
+    }
+
+    public void previousStation() {
+        if (currentStationNumber > 0) {
+            currentStationNumber --;
+        }
+        else {
+            setMaxStationNumber();
         }
     }
 
--- src/test/java/ru/netology/oopp2/RadioTest.java
+++ src/test/java/ru/netology/oopp2/RadioTest.java
@@ -8,6 +8,19 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class RadioTest {
 
     @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/setRadio_station.csv")
+    public void shouldSetStationNumber(int newStationNumber, int expected) {
+        Radio cond = new Radio();
+
+        cond.setCurrentStationNumber(newStationNumber);
+
+
+        int actual = cond.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/setRadio_volume.csv")
     public void shouldSetVolume(int newCurrentVolume, int expected) {
         Radio cond = new Radio();
@@ -17,6 +30,118 @@ public class RadioTest {
 
         int actual = cond.getCurrentVolume();
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/nextRadio_station.csv")
+    public void shouldNextStation(int newStationNumber, int expected) {
+        Radio cond = new Radio();
+        cond.setCurrentStationNumber(newStationNumber);
+
+        cond.nextStation();
+
+
+        int actual = cond.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/previousRadio_station.csv")
+    public void shouldPreviousStation(int newStationNumber, int expected) {
+        Radio cond = new Radio();
+        cond.setCurrentStationNumber(newStationNumber);
+
+        cond.previousStation();
+
+
+        int actual = cond.getCurrentStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/increaseRadio_volume.csv")
+    public void shouldIncreaseVolume(int newCurrentVolume, int expected) {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(newCurrentVolume);
+
+        cond.increaseVolume();
+
+
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/decreaseRadio_volume.csv")
+    public void shouldDecreaseVolume(int newCurrentVolume, int expected) {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(newCurrentVolume);
+
+        cond.decreaseVolume();
+
+
+        int actual = cond.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/multipleNextRadio_station.csv")
+    public void multipleNextStation(int newStationNumber, int expected, int steps) {
+        Radio cond = new Radio();
+        cond.setCurrentStationNumber(newStationNumber);
+
+        for (int i = 0; i < steps; i++) {
+            cond.nextStation();
+        }
+
+        int actual = cond.getCurrentStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/multiplePreviousRadio_station.csv")
+    public void multiplePreviousStation(int newStationNumber, int expected, int steps) {
+        Radio cond = new Radio();
+        cond.setCurrentStationNumber(newStationNumber);
+
+        for (int i = 0; i < steps; i++) {
+            cond.previousStation();
+        }
+
+        int actual = cond.getCurrentStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/multipleIncreaseRadio_volume.csv")
+    public void multipleIncreasesVolume(int newCurrentVolume, int expected, int increases) {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(newCurrentVolume);
+
+        for (int i = 0; i < increases; i++) {
+            cond.increaseVolume();
+        }
+
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/multipleDecreaseRadio_volume.csv")
+    public void multipleDecreasesVolume(int newCurrentVolume, int expected, int decreases) {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(newCurrentVolume);
+
+        for (int i = 0; i < decreases; i++) {
+            cond.decreaseVolume();
+        }
+
+        int actual = cond.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
 
@@ -68,64 +193,6 @@ public class RadioTest {
 //        Assertions.assertEquals(expected, actual);
 //    }
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/increaseRadio_volume.csv")
-    public void shouldIncreaseVolume(int newCurrentVolume, int expected) {
-        Radio cond = new Radio();
-        cond.setCurrentVolume(newCurrentVolume);
 
-        cond.increaseVolume();
-
-
-        int actual = cond.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    ;
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/decreaseRadio_volume.csv")
-    public void shouldDecreaseVolume(int newCurrentVolume, int expected) {
-        Radio cond = new Radio();
-        cond.setCurrentVolume(newCurrentVolume);
-
-        cond.decreaseVolume();
-
-
-        int actual = cond.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    ;
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/multipleIncreaseRadio_volume.csv")
-    public void multipleIncreasesVolume(int newCurrentVolume, int expected, int increases) {
-        Radio cond = new Radio();
-        cond.setCurrentVolume(newCurrentVolume);
-
-        for (int i = 0; i < increases; i++) {
-            cond.increaseVolume();
-        }
-
-        int actual = cond.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/multipleDecreaseRadio_volume.csv")
-    public void multipleDecreasesVolume(int newCurrentVolume, int expected, int decreases) {
-        Radio cond = new Radio();
-        cond.setCurrentVolume(newCurrentVolume);
-
-        for (int i = 0; i < decreases; i++) {
-            cond.decreaseVolume();
-        }
-
-        int actual = cond.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);
-    }
 
 }
